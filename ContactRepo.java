3da2df4a3fe766127b2200bf003b5498ee3933c3
Shivangi Ranjan <shivangiec1098@gmail.com>
2021-12-20 23:36:39 +0530

    It contains class methods and class instances related to add contact details in adressbook

---
+++ ContactRepo.java
@@ -0,0 +1,24 @@
+package com;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactRepo {
+    private static ContactRepo instance;
+    private List<Contacts> contactList = new ArrayList<>();
+
+    private ContactRepo() {
+    }
+
+    public static ContactRepo getInstance() {
+        if (instance == null) {
+            instance = new ContactRepo();
+        }
+        return instance;
+    }
+
+    void add(Contacts contact) {
+        contactList.add(contact);
+    }
+
+}
